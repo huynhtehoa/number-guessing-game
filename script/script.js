d47ef4cf72bd98ea6880257869244e104adc4f38
Hoa <huynhtehoa@gmail.com>
2019-05-15 23:48:04 +0700

    add history

--- script/script.js
+++ script/script.js
@@ -2,38 +2,65 @@ let remainingGuess = 10;
 
 document.getElementById("remaining-guess").innerHTML = remainingGuess;
 
+let correctNumber = Math.floor(Math.random() * 100);
+
 function guessingGame() {
+  let guessOutcome;
+  let displayCorrect;
+  let displayWrong;
+
   let guessInput = parseInt(document.getElementById("guess-input").value);
-  let correctNumber = Math.floor(Math.random() * 100);
 
   if (remainingGuess == 0) {
-    return document.getElementById("out-of-turn").innerHTML = "Out of turn. Press reset!";
+    return document.getElementById("out-of-turn").innerHTML = "No more turn. Press Alt + F4!";
   }
   if (guessInput < correctNumber) {
     guessOutcome = "Your number is too low";
-    document.getElementById("guess-wrong").style.display = "block";
+    displayWrong = "block";
+    displayCorrect = "none";
   }
   if (guessInput > correctNumber) {
     guessOutcome = "Your number is too high";
-    document.getElementById("guess-wrong").style.display = "block";
+    displayWrong = "block";
+    displayCorrect = "none";
 
   }
   if (guessInput == correctNumber) {
-    document.getElementById("guess-success").style.display = "block";
-  }
+    displayCorrect = "block";
+    displayWrong = "none";
 
+  }
+  document.getElementById("guess-wrong").style.display =  displayWrong;
+  document.getElementById("guess-success").style.display = displayCorrect;
   document.getElementById("outcome").innerHTML = guessOutcome;
   document.getElementById("guess-input").value = "";
   document.getElementById("remaining-guess").innerHTML = parseInt(remainingGuess) - 1;
 
   remainingGuess -= 1;
+
+//output history
+  let arrayHistory = []
+
+  arrayHistory.push(guessInput)
+
+  arrayHistory.forEach(function(element) {
+    
+    if (element == guessInput) {
+      console.log("fuckyou")
+    }
+
+    document.getElementById("history").innerHTML += element + " "
+  } 
+  )
 }
+
+
 
 function resetGame() {
   remainingGuess = 10;
   document.getElementById("remaining-guess").innerHTML = remainingGuess;
-  document.getElementById("out-of-turn").innerHTML = " ";
-  guessOutcome  = "";
+  document.getElementById("out-of-turn").innerHTML = "";
+  document.getElementById("outcome").innerHTML = "";
   document.getElementById("guess-wrong").style.display = "none";
   document.getElementById("guess-success").style.display = "none";
 }
